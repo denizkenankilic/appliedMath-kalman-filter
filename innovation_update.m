9b43c8ebbefa61f120d634d1274c4b29d2faa7c0
Deniz Kenan Kilic <deniz.kenan.kilic@gmail.com>
2021-06-13 22:20:47 +0300

    Update innovation_update.m

--- innovation_update.m
+++ innovation_update.m
@@ -1,4 +1,4 @@
 function [xnew, Pnew] = innovation_update(xpred, Ppred, nu, S, H)
-K = Ppred*H'*inv(S); %% Kalman gain
-xnew = xpred + K*nu; %% new state
-Pnew = Ppred - K*S*K'; %% new covariance
+K = Ppred * H' * inv(S); %% Kalman gain
+xnew = xpred + K * nu; %% new state
+Pnew = Ppred - K * S * K'; %% new covariance
